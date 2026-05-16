cec20018451a42bc16d7b513f02b6826c39cced0
Mayyte <carromayte@gmail.com>
2026-05-16 12:30:23 -0300

    feat(events): change events blocks

--- src/blocks/events.ts
+++ src/blocks/events.ts
@@ -3,38 +3,56 @@
 namespace ButiaEvents {
 
     //% blockId="butia_evt_distance"
-    //% block="Sensor de distancia en %connector  %op  %threshold cm"
-    //% threshold.defl=20 threshold.min=1 threshold.max=400
+    //% block="Cuando la distancia en %connector sea %op %threshold cm"
+    //% threshold.defl=20 threshold.min=1 threshold.max=100
     //% weight=65
     //% group="Eventos"
-    export function onDistance(connector: Butia.Connector, op: Comparison, threshold: number, handler: () => void): void {
+    export function onDistance(
+        connector: Butia.Connector,
+        op: Comparison,
+        threshold: number,
+        handler: () => void
+    ): void {
         Butia.RobotDriver.getCurrentRobot().onDistance(connector, op, threshold, handler);
     }
 
     //% blockId="butia_evt_light"
-    //% block="Sensor de luz en %connector %op %threshold"
+    //% block="Cuando la luz en %connector sea %op %threshold"
     //% threshold.defl=20 threshold.min=1 threshold.max=100
     //% weight=60
     //% group="Eventos"
-    export function onLight(connector: Butia.Connector, op: Comparison, threshold: number, handler: () => void): void {
+    export function onLight(
+        connector: Butia.Connector,
+        op: Comparison,
+        threshold: number,
+        handler: () => void
+    ): void {
         Butia.RobotDriver.getCurrentRobot().onLight(connector, op, threshold, handler);
     }
 
     //% blockId="butia_evt_gray"
-    //% block="Sensor de grises en %connector %op %threshold"
+    //% block="Cuando el sensor de grises en %connector sean %op %threshold"
     //% threshold.defl=20 threshold.min=1 threshold.max=100
-    //% weight=60
+    //% weight=55
     //% group="Eventos"
-    export function onGray(connector: Butia.Connector, op: Comparison, threshold: number, handler: () => void): void {
+    export function onGray(
+        connector: Butia.Connector,
+        op: Comparison,
+        threshold: number,
+        handler: () => void
+    ): void {
         Butia.RobotDriver.getCurrentRobot().onGray(connector, op, threshold, handler);
     }
 
     //% blockId="butia_evt_button"
-    //% block="Sensor de Botón en %connector al %state"
+    //% block="Cuando se %state el botón en %connector"
     //% weight=70
     //% group="Eventos"
-    export function onButton(connector: Butia.Connector, state: ButtonState, handler: () => void): void {
+    export function onButton(
+        state: ButtonState,
+        connector: Butia.Connector,
+        handler: () => void
+    ): void {
         Butia.RobotDriver.getCurrentRobot().onConnectorButton(connector, state, handler);
     }
-
 }
